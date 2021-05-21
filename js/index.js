2cbea9946c11f40085de69b65fa638468dee2954
tiffzhao45 <75640533+tiffzhao45@users.noreply.github.com>
2021-05-20 20:14:17 -0500

    hw19

---
+++ js/index.js
@@ -0,0 +1,24 @@
+//Makes sure linked correctly 
+console.log("External Javascript & JQuery loaded correctly!");
+//Get the button:
+mybutton = document.getElementById("myBtn");
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+//Alert
+About = document.getElementById("About");
+About.onmouseleave= function () {HireMeAlert()};
+function HireMeAlert () {
+  alert("Hire Me!");
+}
